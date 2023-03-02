b050ec946598223a255192e65ca9f908511a6e2f
Darrenwwk <darrenwongwwk@gmail.com>
2023-03-02 14:52:08 +0800

    message changed

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ export default function Home({
         <title>Darren Blog</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>My name is Darren. I am a full stack developer and you may contact me on <a href='https://www.facebook.com/waikhan.wong/'>Facebook</a></p>
+        <p>My name is Darren. I am currently learning nextjs</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
